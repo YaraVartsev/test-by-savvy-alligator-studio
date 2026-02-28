de810110888e3256041a6f2e5398c4b42f9ed6e5
YaraVartsev <futbik1990@gmail.com>
2026-02-28 22:15:06 +0200

    змінив slider додав стилі тексту

--- js/main.js
+++ js/main.js
@@ -1,9 +1,69 @@
-$(function() {
+// $(function() {
 
-  $('.slider__inner').slick({
-    dots: true,
-    arrows: false,
-  });
+//   $('.slider__inner').slick({
+//     dots: true,
+//     arrows: false,
+//   });
 
 
-});
+// });
+
+
+ let myImageSwiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      autoHeight: true,
+
+      slidesPerView: 1,
+
+      centeredSlides: true,
+
+      loop: true,
+
+      // controller:{
+      //   control: myTextSwiper,
+      // }
+
+      // breakpoints: {
+      //   480: {
+      //     slidesPerView: 1,
+      //   },
+      //   1280: {
+      //     slidesPerView: 2,
+      //   }
+      // }
+
+
+
+      
+      });
+
+
+
+    let myTextSwiper = new Swiper(".mySwiper--text", {
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      autoHeight: true,
+
+      slidesPerView: 1,
+
+      centeredSlides: true,
+
+      loop: true,
+
+      // controller:{
+      //   control: myTextSwiper,
+      // }
+
+    });
+    
+    myImageSwiper.controller.control = myTextSwiper;
+    myTextSwiper.controller.control = myImageSwiper;
